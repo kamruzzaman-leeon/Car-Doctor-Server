66e6072a9f6ab7585db51c60baa76f62b92415a4
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-14 00:41:42 +0600

    jwt with creditional added

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
+
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
-
-console.log(process.env.DB_PASS)
+// console.log(process.env.DB_PASS)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jfba5ry.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,6 +41,24 @@ async function run() {
         const serviceCollection = client.db('carDoctor').collection('services');
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
+        //auth related api
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            console.log(user)
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+
+            res
+                .cookie('token', token, {
+                    httpOnly: true,
+                    secure: false,
+
+                    sameSite: false
+                })
+                .send({success: true})
+        })
+
+
+        //services
         app.get('/services', async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
@@ -55,6 +82,7 @@ async function run() {
         // bookings 
         app.get('/bookings', async (req, res) => {
             console.log(req.query.email);
+            console.log('token: ', req.cookies.token)
             let query = {};
             if (req.query?.email) {
                 query = { email: req.query.email }
