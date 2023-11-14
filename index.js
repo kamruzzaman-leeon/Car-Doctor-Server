b5dda9b0c77cfb20d4beb96c1625dff74e2b5c9b
kamruzzaman-leeon <leeon.ewubd@gmail.com>
2023-11-14 17:29:40 +0600

    server update

--- index.js
+++ index.js
@@ -33,6 +33,23 @@ const client = new MongoClient(uri, {
     }
 });
 
+
+//middleware our own
+const logger = async (req, res, next) => {
+    console.log('called', req.host, req.originalUrl)
+    next();
+}
+
+const verifyToken = async(req, res, next)=>{
+    const token = req.cookies?.token;
+    console.log('value of token in middleware', token)
+    if(!token){
+        return res.status(401).send({message: 'forbidden'})
+    }
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+    next();
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
@@ -42,7 +59,7 @@ async function run() {
         const bookingCollection = client.db('carDoctor').collection('bookings');
 
         //auth related api
-        app.post('/jwt', async (req, res) => {
+        app.post('/jwt', logger,verifyToken, async (req, res) => {
             const user = req.body;
             console.log(user)
             const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
@@ -54,12 +71,12 @@ async function run() {
 
                     sameSite: false
                 })
-                .send({success: true})
+                .send({ success: true })
         })
 
 
         //services
-        app.get('/services', async (req, res) => {
+        app.get('/services', logger, verifyToken, async (req, res) => {
             const cursor = serviceCollection.find();
             const result = await cursor.toArray();
             res.send(result);
